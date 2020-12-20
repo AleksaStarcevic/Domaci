0cfcb708273d7c9afdfc68f324d4ba84cd9d118f
AleksaStarcevic <aleksastarcevic1@gmail.com>
2020-12-20 20:28:22 +0100

    alert

---
+++ epos-git-domaci-master/js/main.js
@@ -0,0 +1,19 @@
+var kart = document.querySelector("#kart");
+var kart2 = document.querySelector("#kart2");
+var kart3 = document.querySelector("#kart3");
+
+var date = new Date();
+
+kart.addEventListener('click',function(){
+ alert(date);
+});
+
+kart2.addEventListener('click',function(){
+ alert(date);
+});
+kart3.addEventListener('click',function(){
+ alert(date);
+});
+
+ 
+ 
